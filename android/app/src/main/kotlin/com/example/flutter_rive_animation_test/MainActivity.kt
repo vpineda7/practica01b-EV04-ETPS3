3a62d182f3f456fe331c01ed92831aa1dc3e9ca3
vpineda7 <vladimir.pineda7@gmail.com>
2022-05-21 15:59:24 -0600

    primer y funcional app usa fir file

---
+++ android/app/src/main/kotlin/com/example/flutter_rive_animation_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_rive_animation_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo Rive Animation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+       home: RiveDemo(),
+    );
+  }
+}
+
+
+class RiveDemo extends StatefulWidget {
+  //  RiveDemo({ Key key}) : super(key: key);
+   const RiveDemo({Key? key}) : super(key: key);
+   
+  @override
+  _RiveDemoState createState() => _RiveDemoState();
+}
+
+class _RiveDemoState extends State<RiveDemo> {
+  bool get isPlaying => _controller.isActive;
+  late Artboard _riveArtboard ;
+  late RiveAnimationController _controller;
+  @override
+  void initState() {
+    super.initState();
+
+    rootBundle.load('assets/smile.riv').then(
+      (data) async {
+        final file = RiveFile.import(data);
+        final artboard = file.mainArtboard;
+        setState(() => _riveArtboard = artboard);
+      },
+    );
+  }
+
+  void _launchRocket() {
+    _riveArtboard.addController(_controller = SimpleAnimation('Smile'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _riveArtboard == null
+            ? const SizedBox()
+            : Rive(artboard: _riveArtboard),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _launchRocket,
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
+
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+
+  
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
+
